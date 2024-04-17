6abe9411824ef760f95a15c7a98b0e1d54eaf867
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-04-17 00:55:09 -0300

    advances

---
+++ src/tests/testMigrate.js
@@ -0,0 +1,16 @@
+const sequelize = require("../utils/connection");
+
+const testMigrate = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    console.log('DB reset 👌😊');
+
+    process.exit()
+
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
+testMigrate()
